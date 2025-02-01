093bac7b8e16e190de0291008e4b14af95d5b486
Seth Harlaar <sjharlaar@gmail.com>
2025-02-01 07:46:48 -0500

    asfd

--- src/app/api/login/redirect/route.ts
+++ src/app/api/login/redirect/route.ts
@@ -1,4 +1,5 @@
 import { User, UserSearchCriteria } from "@/models/User";
+import { RESPONSE_LIMIT_DEFAULT } from "next/dist/server/api-utils";
 import { NextRequest, NextResponse } from "next/server";
 const {google} = require('googleapis');
 
@@ -10,50 +11,46 @@ export async function GET(request: NextRequest) {
   );
 
   try {
+    // get google auth response
     const params = request.nextUrl.searchParams;
     const code = params.get('code')
-  
-    try {
     const {tokens} = await oauth2Client.getToken(code);
+    const ticket = await oauth2Client.verifyIdToken({
+      idToken: tokens.id_token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
 
-      const ticket = await oauth2Client.verifyIdToken({
-        idToken: tokens.id_token,
-        audience: process.env.GOOGLE_CLIENT_ID,
-      });
+    const payload = ticket.getPayload();
 
-      const payload = ticket.getPayload();
+    console.log(payload);
 
-      console.log(payload);
-
-
-      let res = NextResponse.redirect(`${process.env.BASE_URL}/home`);
-    } catch {
-
-      let res = NextResponse.redirect(`${process.env.BASE_URL}/404`);
-    }
-
-
-
-    // create user obj and save
-    let userLoggingIn = await User.GetUser(new UserSearchCriteria({GoogleId: userProfile.googleId}));
+    // find or create user
+    let userLoggingIn = await User.GetUser(new UserSearchCriteria({GoogleId: payload['sub']}));
     console.log('userLoggingIn', userLoggingIn);
-    if(userLoggingIn){
-      return res;
 
-    } else {
-      let names = userProfile.name.split(' ');
-      let firstName = names[0];
-      let lastName = names[names.length - 1];
-      let createdUser = new User(firstName, lastName, userProfile.email, userProfile.googleId);
+    if(!userLoggingIn){
+      let createdUser = new User(payload['given_name'], payload['family_name'], payload['email'], payload['sub']);
       await createdUser.SaveUser();
       console.log('created new', createdUser);
-      if(createdUser.UserId > 0){
-        let res = NextResponse.redirect(`${process.env.BASE_URL}/home`);
-        return res;
+      if(createdUser.UserId > 0){ 
+        throw new Error('Could not create new user');
       }
     }
 
+    // creat jwt
+    var x = 
+
+    let res = NextResponse.redirect(`${process.env.BASE_URL}/home`);
+    res.cookies.set('user-profile', JSON.stringify(userProfile), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 60 * 60 * 24 * 7, // 1 week
+      path: '/',
+      sameSite: "strict",
+    });
+
     return res;
+
   } catch (error) {
     console.log(error);
     return Response.redirect(`${process.env.BASE_URL}/404`);
--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export function middleware(request: NextRequest) {
   }
   return NextResponse.next();
 }
- 
+
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: '/',
