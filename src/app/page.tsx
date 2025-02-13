fca40875951ffb745c4a5218d3af773465536a0d
Seth Harlaar <sjharlaar@gmail.com>
2025-02-12 20:10:13 -0500

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,52 +11,59 @@ export default async function Home() {
   const daysOfWeek: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const mealPlans = await MealPlan.GetMealPlans([5]);
 
-  console.log(await GetCurrentUser());
+  const user = await GetCurrentUser();
 
-  return (
-    <>
-      <PageTitle titleText={mealPlans[0].Name}/>
-      <div className="page-splitter">
-        {/* Left side */}
-        <div id="meal-plan-days" className="left">
-          <select id="meal-plan-selector">
-            {mealPlans.map((mealPlan, index) => {
-              return <option key={index} value={index}>
-                {mealPlan.Name}
-              </option>
-            })}
-          </select>
-
-          {mealPlans[0].Days.map((day, index) => {
-            return <div key={index} className="day">
-
-              <h1>{daysOfWeek[index]}</h1>
-              {Array.from(day.Meals.entries()).map(([mealTime, mealData], index) => {
-                return <MealListing mealTitle={mealData.name} mealTime={mealTime} key={index}/>
+  if(user){
+    return (
+      <>
+        <PageTitle titleText={mealPlans[0].Name}/>
+        <div className="page-splitter">
+          {/* Left side */}
+          <div id="meal-plan-days" className="left">
+            <select id="meal-plan-selector">
+              {mealPlans.map((mealPlan, index) => {
+                return <option key={index} value={index}>
+                  {mealPlan.Name}
+                </option>
               })}
-            </div>
-          })}
-        </div>
-        {/* Right side */}
-        <div id="meal-plans" className="right">
-          <h1>Meal Plans</h1>
-          <div className="meal-plan-list">
-            {mealPlans.map((mealPlan, index) => {
-              return <React.Fragment key={index}>
-                <div className="meal-plan">
-                  <h2>{mealPlan.Name}</h2>
-                  <h3>asdf</h3>
-                </div>
-                <hr/>
-              </React.Fragment>
+            </select>
+
+            {mealPlans[0].Days.map((day, index) => {
+              return <div key={index} className="day">
+
+                <h1>{daysOfWeek[index]}</h1>
+                {Array.from(day.Meals.entries()).map(([mealTime, mealData], index) => {
+                  return <MealListing mealTitle={mealData.name} mealTime={mealTime} key={index}/>
+                })}
+              </div>
             })}
-            <div id="new-meal-plan-button">
-              <div></div>
+          </div>
+          {/* Right side */}
+          <div id="meal-plans" className="right">
+            <h1>Meal Plans</h1>
+            <div className="meal-plan-list">
+              {mealPlans.map((mealPlan, index) => {
+                return <React.Fragment key={index}>
+                  <div className="meal-plan">
+                    <h2>{mealPlan.Name}</h2>
+                    <h3>asdf</h3>
+                  </div>
+                  <hr/>
+                </React.Fragment>
+              })}
+              <div id="new-meal-plan-button">
+                <div></div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+
+    return (
+      <>not logged in</>
+    );
+  }
 }
 
