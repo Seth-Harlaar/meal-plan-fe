67072f00919ad7f25f7bb919e2c1fe1e748d44da
Seth Harlaar <sjharlaar@gmail.com>
2025-03-09 06:09:56 -0400

    a

--- src/app/mealplan/list/MealPlanListing.tsx
+++ src/app/mealplan/list/MealPlanListing.tsx
@@ -5,12 +5,12 @@ import { StarIcon } from "@/components/Icons";
 import { useState } from "react";
 
 import './MealPlanListing.css';
+import { MealTime } from "@/models/enums/MealTime";
 
 export default function MealPlanListing(
   {mealPlanDataList, primaryMealPlanId} :
   {mealPlanDataList: MealPlanResultType[], primaryMealPlanId: number}
 ){
-
   const [primaryMealPlanIDState, setPrimaryMealPlanIDState] = useState<number>(primaryMealPlanId);
 
   function handleMealCardClick(mealPlanId: number){
@@ -22,11 +22,13 @@ export default function MealPlanListing(
     <>
       {mealPlanDataList.map((mealplan, index) => {
         const isPrimary = mealplan.id == primaryMealPlanIDState;
+
         return <div className="card mealplan-card" key={index} onClick={() => handleMealCardClick(mealplan.id)}>
-            {mealplan.name}
+            <h1>{mealplan.name}</h1>
             {isPrimary && 
               <StarIcon />
             }
+            <h3>Meals: </h3>
           </div>
       })}
     </>
