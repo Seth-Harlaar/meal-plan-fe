94731731632c0d5dfa8494bbedc3b4b5aa222439
Seth Harlaar <sjharlaar@gmail.com>
2025-03-25 20:05:52 -0400

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,10 +37,6 @@ export default async function Home() {
     }));
   }
 
-  const session = await getSession(); 
-
-  console.log(session);
-
   return (
     <>
       <div className="page-splitter">
